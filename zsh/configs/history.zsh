0556fc4c303da458ad4baa403977863fcbb659cd
Alex Gerstein <gerstein.alex@gmail.com>
2017-04-24 13:24:41 -0700

    Search through history from command line in ZSH

--- zsh/configs/history.zsh
+++ zsh/configs/history.zsh
@@ -2,3 +2,10 @@ setopt hist_ignore_all_dups inc_append_history
 HISTFILE=~/.zhistory
 HISTSIZE=4096
 SAVEHIST=4096
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
