59ef53edcb86574d9739180a4a6b32367001645e
Alex Gerstein <gerstein.alex@gmail.com>
2017-04-24 03:19:41 -0700

    Fix path for subl and other command tools

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -9,4 +9,10 @@ fi
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
+# Add bin for subl command completion
+PATH="$PATH:$HOME/bin"
+
+# Add local/bin cli tools
+export PATH=/usr/local/bin:$PATH
+
 export -U PATH
